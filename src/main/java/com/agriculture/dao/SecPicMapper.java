0eeea159cfa2baa4c1f6c13fe40a09a40aaae38f
matchstick9505 <1749330778@qq.com>
2020-02-28 19:50:12 +0800

    图片上传1.0.0

---
+++ src/main/java/com/agriculture/dao/SecPicMapper.java
@@ -0,0 +1,14 @@
+package com.agriculture.dao;
+
+
+import com.agriculture.pojo.SecPic;
+
+public interface SecPicMapper {
+
+    /**
+     * 将上传图片的位置存到数据库
+     * @param secPic
+     * @return
+     */
+    int addPiC(SecPic secPic);
+}
---
+++ src/main/java/com/agriculture/pojo/SecPic.java
@@ -0,0 +1,96 @@
+package com.agriculture.pojo;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class SecPic implements Serializable {
+    private Integer id;
+
+    private String name;
+
+    private String info;
+
+    private String path;
+
+    private Float size;
+
+    private Boolean disabled;
+
+    private Date createtime;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info == null ? null : info.trim();
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path == null ? null : path.trim();
+    }
+
+    public Float getSize() {
+        return size;
+    }
+
+    public void setSize(Float size) {
+        this.size = size;
+    }
+
+    public Boolean getDisabled() {
+        return disabled;
+    }
+
+    public void setDisabled(Boolean disabled) {
+        this.disabled = disabled;
+    }
+
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append(", info=").append(info);
+        sb.append(", path=").append(path);
+        sb.append(", size=").append(size);
+        sb.append(", disabled=").append(disabled);
+        sb.append(", createtime=").append(createtime);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}
